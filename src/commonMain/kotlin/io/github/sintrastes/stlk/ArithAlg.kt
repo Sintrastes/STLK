3e64d080b17a7cd9ef75a9dd96e87deb32d4398c
Nathan Bedell <nbedell@tulane.edu>
2022-07-11 19:46:53 -0400

    Remove LambdaAlg superclass from IntArithAlg

--- src/commonMain/kotlin/io/github/sintrastes/stlk/ArithAlg.kt
+++ src/commonMain/kotlin/io/github/sintrastes/stlk/ArithAlg.kt
@@ -3,13 +3,13 @@ package io.github.sintrastes.stlk
 import kotlin.reflect.KType
 
 /** Object algebra for arithmetic operations on integers. */
-interface IntArithAlg<F> : LambdaAlg<F> {
+interface IntArithAlg<F> {
     fun int(x: Int): Apply<F, Int>
     operator fun Apply<F, Int>.plus(other: Apply<F, Int>): Apply<F, Int>
     operator fun Apply<F, Int>.times(other: Apply<F, Int>): Apply<F, Int>
     operator fun Apply<F, Int>.minus(other: Apply<F, Int>): Apply<F, Int>
 
-    object Interpreter : LambdaAlg<IdOf> by LambdaAlg.Interpreter, IntArithAlg<IdOf> {
+    object Interpreter : IntArithAlg<IdOf> {
         override fun int(x: Int): Apply<IdOf, Int> = Id(x)
 
         override fun Apply<IdOf, Int>.minus(other: Apply<IdOf, Int>) = Id(fix() - other.fix())
@@ -19,7 +19,7 @@ interface IntArithAlg<F> : LambdaAlg<F> {
         override fun Apply<IdOf, Int>.plus(other: Apply<IdOf, Int>) = Id(fix() + other.fix())
     }
 
-    object Serializer : LambdaAlg<ConstOf<RawExpr>> by LambdaAlg.Serializer, IntArithAlg<ConstOf<RawExpr>> {
+    object Serializer : IntArithAlg<ConstOf<RawExpr>> {
         override fun int(x: Int): Apply<ConstOf<RawExpr>, Int> = Const(
             RawExpr.Const(x)
         )
@@ -52,8 +52,6 @@ interface IntArithAlg<F> : LambdaAlg<F> {
     object Deserializer : ExprDeserializer {
         override fun <A : Any> deserialize(type: KType, raw: RawExpr, rec: ExprDeserializer): A? {
             return when (raw) {
-                is RawExpr.App -> null
-                is RawExpr.AppOp -> null
                 is RawExpr.Const -> when (raw.value) {
                     is Int -> {
                         raw.value as? A
@@ -66,8 +64,7 @@ interface IntArithAlg<F> : LambdaAlg<F> {
                     "times" -> ({ x: Int, y: Int -> x * y }) as? A
                     else -> null
                 }
-                is RawExpr.Lam -> null
-                is RawExpr.Var -> null
+                else -> null
             }
         }
     }
