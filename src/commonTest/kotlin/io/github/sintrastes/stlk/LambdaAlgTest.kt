0f5b4afcfafcab78dd78b456c9e982fe00aca56b
Nathan Bedell <nbedell@tulane.edu>
2022-07-10 20:49:15 -0400

    Add a deserializer test.

--- src/commonTest/kotlin/io/github/sintrastes/stlk/LambdaAlgTest.kt
+++ src/commonTest/kotlin/io/github/sintrastes/stlk/LambdaAlgTest.kt
@@ -5,16 +5,29 @@ import io.kotest.matchers.shouldBe
 
 class LambdaAlgTest : FunSpec({
     test("Test integer arithmetic interpreter") {
-        fun <F> IntArithAlg<F>.example(): Apply<F, (Int) -> Int> {
-            return func { x ->
-                x * int(5) + int(6)
-            }
-        }
-
         val test = IntArithAlg.Interpreter
             .example()
             .fix()
 
         test(2) shouldBe 16
     }
+
+    test("Deserializer test") {
+        val raw = LambdaAlg.Serializer.lambdaExample()
+            .fix()
+
+        val example = LambdaAlg.deserialize<(Int) -> Int>(raw)!!
+
+        example(2) shouldBe 2
+    }
 })
+
+fun <F> LambdaAlg<F>.lambdaExample(): Apply<F, (Int) -> Int> {
+    return func { x -> x }
+}
+
+fun <F> IntArithAlg<F>.example(): Apply<F, (Int) -> Int> {
+    return func { x ->
+        x * int(5) + int(6)
+    }
+}
