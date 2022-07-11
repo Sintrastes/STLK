a96d91436a66feb746f5c78256f552b9ab1e7741
Nathan Bedell <nbedell@tulane.edu>
2022-07-10 20:50:31 -0400

    Make use of our pattern matcher in deserialize.

--- src/commonMain/kotlin/io/github/sintrastes/stlk/LambdaAlg.kt
+++ src/commonMain/kotlin/io/github/sintrastes/stlk/LambdaAlg.kt
@@ -1,6 +1,8 @@
 package io.github.sintrastes.stlk
 
-import kotlin.reflect.KClass
+import io.github.sintrastes.stlk.typeutils.FunMatcher
+import io.github.sintrastes.stlk.typeutils.patternMatchFunType
+import kotlin.reflect.*
 
 /**
  * The base object algebra for STLK. Provides the simply-typed lambda calculus
@@ -40,26 +42,51 @@ interface LambdaAlg<F> {
          * Deserializer for deserializing a value from a RawExpr for the [LambdaAlg] DSL
          * using open recursion to that deserializers for other algebras can be built
          * on top of this one. */
-        fun <A : Any> deserialize(clazz: KClass<A>, raw: RawExpr, rec: ExprDeserializer = ExprDeserializer.Empty): A? {
+        inline fun <reified A : Any> deserialize(raw: RawExpr, rec: ExprDeserializer = ExprDeserializer.Empty): A? {
+            val type = typeOf<A>()
+            return deserialize(type, raw, rec)
+        }
+
+        /**
+         * Deserializer for deserializing a value from a RawExpr for the [LambdaAlg] DSL
+         * using open recursion to that deserializers for other algebras can be built
+         * on top of this one.
+         *
+         * Assumption: [A] must be the same type as [type]. May cause strange issues otherwise
+         *  due to type erasure.
+         */
+        fun <A : Any> deserialize(type: KType, raw: RawExpr, rec: ExprDeserializer = ExprDeserializer.Empty): A? {
             return when (raw) {
                 is RawExpr.App -> {
-                    rec.deserialize(clazz, raw.f)?.let { f ->
-                        rec.deserialize(clazz, raw.x)?.let { x ->
-                            TODO()
-                        }
-                    }
+                    type.patternMatchFunType(
+                        onMatch = object : FunMatcher<A?> {
+                            override fun <X : Any, Y : Any> match(
+                                inClass: KClass<X>,
+                                outClass: KClass<Y>,
+                                inType: KType,
+                                outType: KType
+                            ): A? {
+                                return rec.deserialize<(X) -> Y>(type, raw.f)?.let { f ->
+                                    rec.deserialize<X>(inType, raw.x)?.let { x ->
+                                        f(x) as A
+                                    }
+                                }
+                            }
+                        },
+                        onMiss = null
+                    )
                 }
                 is RawExpr.AppOp -> {
-                    rec.deserialize(clazz, raw)
+                    rec.deserialize(type, raw)
                 }
                 is RawExpr.Const -> {
-                    rec.deserialize(clazz, raw)
+                    rec.deserialize(type, raw)
                 }
                 is RawExpr.Var -> {
-                    rec.deserialize(clazz, raw)
+                    rec.deserialize(type, raw)
                 }
                 is RawExpr.CustomOp -> {
-                    rec.deserialize(clazz, raw)
+                    rec.deserialize(type, raw)
                 }
             }
         }
@@ -71,10 +98,15 @@ interface LambdaAlg<F> {
  *  a specified class.
  */
 interface ExprDeserializer {
-    fun <A : Any> deserialize(clazz: KClass<A>, raw: RawExpr): A?
+    /**
+     * Deserialize the raw expression [raw] to the type [type].
+     *
+     * Assumption: [A] must be the same type as [type].
+     */
+    fun <A : Any> deserialize(type: KType, raw: RawExpr): A?
 
-    object Empty: ExprDeserializer {
-        override fun <A : Any> deserialize(clazz: KClass<A>, raw: RawExpr): A? {
+    object Empty : ExprDeserializer {
+        override fun <A : Any> deserialize(type: KType, raw: RawExpr): A? {
             return null
         }
     }
