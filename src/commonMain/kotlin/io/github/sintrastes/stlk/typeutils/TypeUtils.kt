c69ae548c2eb80afdea8fbaf2630c386adf27191
Nathan Bedell <nbedell@tulane.edu>
2022-07-16 11:45:38 -0400

    Add utility for pattern matching on the type of lists.

--- src/commonMain/kotlin/io/github/sintrastes/stlk/typeutils/TypeUtils.kt
+++ src/commonMain/kotlin/io/github/sintrastes/stlk/typeutils/TypeUtils.kt
@@ -26,6 +26,28 @@ fun <R> KType.patternMatchFunType(
     }
 }
 
+/** Helper function to pattern match on the type of the passed `KType`
+ * against the pattern `List<A>`. */
+fun <R> KType.patternMatchListType(
+    onMatch: ListMatcher<R>,
+    onMiss: R
+): R {
+    val clazz = (this.classifier as? KClass<*>)
+    val isList = clazz!! == List::class
+
+    return if (isList) {
+        val args = this.arguments
+        val arg1 = args[0].type
+
+        onMatch.match(
+            arg1?.classifier as KClass<*>,
+            arg1
+        )
+    } else {
+        onMiss
+    }
+}
+
 fun <R> KType.visitType(
     typeVisitor: TypeVisitor<R>
 ): R = typeVisitor
@@ -41,5 +63,12 @@ interface FunMatcher<R> {
         outClass: KClass<B>,
         inType: KType,
         outType: KType
+    ): R
+}
+
+interface ListMatcher<R> {
+    fun <A : Any> match(
+        elementClass: KClass<A>,
+        elementType: KType,
     ): R
 }
